2b4a979080ef74fefa2683f46cc069f181982ee7
Robert Forster <robert.forster@beamvex.co.uk>
2025-12-27 13:35:57 +0000

    ``` chore: add S3 bucket name and S6 environment variable to docker run command
    Add S3_BUCKET_NAME environment variable with 'openreach-scrapper' value. Add S6_KEEP_ENV=1 environment variable to preserve environment variables in container.
    ```

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,5 +4,7 @@ bash ./builddocker.sh
 docker run --rm -it -p 3001:3001 \
   --name openreach-scrapper \
   -e AWS_PROFILE=512752756525_AdministratorAccess \
+  -e S3_BUCKET_NAME=openreach-scrapper \
+  -e S6_KEEP_ENV=1 \
   -v ~/.aws:/config/.aws \
   openreach-scrapper:latest
