e0e5ec1fbb227ac5674647b15bfa5301c9eea157
Robert Forster <robert.forster@beamvex.co.uk>
2025-12-30 11:24:31 +0000

    ``` chore: add Lambda IAM role and fix role reference in Lambda function
    Add aws_iam_role.lambda resource with assume role policy for Lambda service. Add aws_iam_role_policy_attachment.lambda_basic_execution to attach AWSLambdaBasicExecutionRole policy. Change Lambda function role reference from aws_iam_role.lambda.id to aws_iam_role.lambda.arn. Remove source_code_hash property from aws_lambda_function.process_results. Update terraform state serial from 290 to 307.
    ```

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -19,9 +19,31 @@ data "archive_file" "lambda" {
 
 resource "aws_lambda_function" "process_results" {
   function_name    = "openreach-scrapper-process-results"
-  role             = aws_iam_role.lambda.id
+  role             = aws_iam_role.lambda.arn
   handler          = "index.handler"
   runtime          = "nodejs18.x"
-  source_code_hash = data.archive_file.lambda.source_code_hash
+  
   filename         = data.archive_file.lambda.output_path
+}
+
+resource "aws_iam_role" "lambda" {
+  name = "openreach-scrapper-process-results-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda.name
+  policy_arn  = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
