6d4b2dda0738dca5af756a30512c99168fc4e46e
Robert Forster <robert.forster@beamvex.co.uk>
2025-12-31 13:37:35 +0000

    ``` chore: update results data and add SNS to Lambda subscription
    Update results.json with latest timestamp data and minify JSON formatting. Add localweb script to package.json for local HTTP server. Create SNS topic subscription to trigger process_results Lambda function with required permissions. Update terraform state serial from 438 to 446.
    ```

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -25,3 +25,19 @@ resource "aws_sns_topic_policy" "openreach_scrapper_allow_s3" {
     ]
   })
 }
+
+resource "aws_lambda_permission" "allow_sns_invoke_process_results" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.process_results.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.openreach_scrapper.arn
+}
+
+resource "aws_sns_topic_subscription" "openreach_scrapper_to_process_results" {
+  topic_arn = aws_sns_topic.openreach_scrapper.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.process_results.arn
+
+  depends_on = [aws_lambda_permission.allow_sns_invoke_process_results]
+}
